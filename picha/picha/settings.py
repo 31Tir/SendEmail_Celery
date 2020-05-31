37b8a5b53a997d85e4411adf87cec0d4c09a59cc
mahmood <66196589+31Tir@users.noreply.github.com>
2020-05-31 14:15:08 +0430

    Update settings.py

--- picha/picha/settings.py
+++ picha/picha/settings.py
@@ -115,8 +115,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'jonzhano@gmail.com'
-EMAIL_HOST_PASSWORD = 'pa*Rva8#$qz40X'
+EMAIL_HOST_USER = 'example@gmail.com'
+EMAIL_HOST_PASSWORD = '**your-pssword**'
 
 
 
